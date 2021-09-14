6dcd467546a73d823138ac8b4e1cd76f024effb3
Mykhailo <mykhailo.lytvynenko1@nure.ua>
2021-09-14 21:50:34 +0300

    Data processing and model selection

--- data/train_valid_split.py
+++ data/train_valid_split.py
@@ -4,5 +4,7 @@ data = pd.read_csv('data\healthcare-dataset-stroke-data.csv')
 
 train = data.sample(frac=.8, random_state=42)
 valid = data.drop(train.index).sample(frac=1, random_state=42)
+del train
+
 train.to_csv('data\\train.csv')
 valid.to_csv('data\\val.csv')
