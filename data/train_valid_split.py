e0e0e72974b4fa47176358e57acb80cdf50395a0
Mykhailo <mykhailo.lytvynenko1@nure.ua>
2021-09-14 17:32:48 +0300

    Datasets added

---
+++ data/train_valid_split.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+data = pd.read_csv('data\healthcare-dataset-stroke-data.csv')
+
+train = data.sample(frac=.8, random_state=42)
+valid = data.drop(train.index).sample(frac=1, random_state=42)
+train.to_csv('data\\train.csv')
+valid.to_csv('data\\val.csv')
